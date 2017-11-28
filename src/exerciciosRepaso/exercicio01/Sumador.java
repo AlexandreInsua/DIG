b7bb2425b3e579831e6a00aebc6036a03b9013c8
Alexandre Insua <alexandreinsua@gmail.com>
2017-11-28 10:57:17 +0100

    Reorganiza Ventanas exercicio repaso 1

---
+++ src/exerciciosRepaso/exercicio01/Sumador.java
@@ -0,0 +1,8 @@
+package exerciciosRepaso.exercicio01;
+
+public class Sumador {
+
+	public float sumarNumero(float num1, float num2, float num3, float num4) {
+		return num1 + num2 + num3 + num4;
+	}
+}
